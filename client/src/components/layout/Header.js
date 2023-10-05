61508d13974c75cdb8f25022d4fb75b3a4974f44
subrat-ranjan <swainsubrat975@gmail.com>
2023-10-05 11:59:11 +0530

    styles

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -23,8 +23,8 @@ const Header = () => {
     }
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+                <div className="container-fluid back">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
