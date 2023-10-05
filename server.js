060c475b6d642eb8ba009bafba26219eb23bf0aa
subrat-ranjan <swainsubrat975@gmail.com>
2023-10-05 12:39:43 +0530

    deployment changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoute.js"
 import categoryRoute from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import cors from 'cors'
-
+import path from "path"//given by node bydefault
 
 //dot env
 dotenv.config()
@@ -19,6 +19,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, './client/build')))
+
 
 //routes
 app.use("/api/v1/auth", authRoutes)
@@ -27,10 +29,13 @@ app.use("/api/v1/product", productRoutes)
 
 
 //rest api
-app.get('/', (req, res) => {
-    res.send({
-        message: "welcome to E-com app.."
-    })
+// app.get('/', (req, res) => {
+//     res.send({
+//         message: "welcome to E-com app.."
+//     })
+// })
+app.use('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
 })
 //PORT
 
